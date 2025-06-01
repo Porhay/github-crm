45403014b148e4746751b4595787b89c46aaac48
Denys Mitiachkin <denismityachkin@gmail.com>
2025-06-02 02:11:04 +0300

    feat(crm): improve backend structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { RepositoriesModule } from './repositories/repositories.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { RepositoriesModule } from './modules/repositories/repositories.module';
 import { databaseConfig } from './config/database.config';
 
 @Module({
